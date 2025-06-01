09483e3d60f94bfd0b2c894d3da64124be46b160
Anjali Tomar <anjali.theaj3366@gmail.com>
2025-06-02 01:33:27 +0530

    Add Data and constants

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -1,3 +1,6 @@
+import { words } from "../constants/index.js";
+import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -9,13 +12,45 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
-              <h1>Shaping</h1>
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span className="flex items-center md:gap-3 gap-1 pb-2">
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+
+                        <span>{word.text}</span>
+                      </span>
+                    ))}{" "}
+                  </span>{" "}
+                </span>
+              </h1>
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
+
+              <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                Hii , I'm Anjali, a developer in Noida with a passion for code.{" "}
+              </p>
+              <Button
+                className="md:w-80 md:h-16 w-60 h-12"
+                id="button"
+                text="See my Work"
+              />
             </div>
           </div>
         </header>
+
         {/* right : 3D model  */}
+        <figure>
+          <div className="hero-3d-layout ">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
